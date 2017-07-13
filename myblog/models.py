85c26204e5c69328c8038d6c23da5ce10de29369
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-07-14 00:00:07 +0545

    Creates a new model

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+	title = models.CharField(max_length = 50)
+	image = models.ImageField(upload_to = 'myblog/image/post', default = None)
+	content = models.TextField()
+	published_date = models.DateTimeField(default = timezone.now)
+
+	def __str__(self):
+		return self.title
