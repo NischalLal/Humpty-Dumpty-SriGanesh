7e0ab35d7b799c4d7bfac5bdce84edfd59609fa8
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-07-14 00:18:27 +0545

    Migrated social site model to db

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Project, SocialSite, Contact, About
 
 # Register your models here
 
 admin.site.register(Post)
+admin.site.register(Project)
+admin.site.register(SocialSite)
+admin.site.register(Contact)
+admin.site.register(About)
--- myblog/models.py
+++ myblog/models.py
@@ -11,3 +11,48 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+
+class Project(models.Model):
+	title = models.CharField(max_length = 50)
+	link = models.URLField()
+	image = models.ImageField(upload_to = 'myblog/image/project', default = None)
+	detail = models.TextField()
+	created_on = models.DateTimeField()
+
+
+	def __str__(self):
+		return self.title
+
+
+class SocialSite(models.Model):
+	site_name = models.CharField(max_length = 10)
+	link = models.URLField()
+
+
+	class Meta:
+		verbose_name_plural = 'Social Sites'
+
+	def __str__(self):
+		return self.site_name
+
+
+class Contact(models.Model):
+	message_from_me = models.TextField()
+	subject = models.CharField(max_length = 33)
+	message_from_user = models.TextField()
+
+
+	def __str__(self):
+		return self.subject
+
+	class Meta:
+		verbose_name_plural = 'Contact'
+
+
+class About(models.Model):
+	about_me = models.TextField()
+
+
+	class Meta:
+		verbose_name_plural = 'About'
