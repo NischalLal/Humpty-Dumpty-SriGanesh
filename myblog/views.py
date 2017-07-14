eb93f81f4e3fceba5f9e64c9b4352a645d3040b7
Nischal Lal Shrestha <aakrist666@gmail.com>
2017-07-14 05:47:04 +0545

    Adds more views

--- myblog/views.py
+++ myblog/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
+from .models import Post, Project, SocialSite, About, Contact
 
 # Create your views here.
 
 
 def home(request):
 	return render(request, 'myblog/home.html', {})
+
+
+def about(request):
+	return render(request, 'myblog/about.html', {})
+
+
+def project_list(request):
+	return render(request, 'myblog/project_list.html', {})
+
+
+def project_detail(request):
+	return render(request, 'myblog/project_detail.html')
+
+
+def post_list(request):
+	return render(request, 'myblog/post_list.html', {})
+
+
+def post_detail(request):
+	return render(request, 'myblog/post_detail.html', {})
+
+
+def contact(request):
+	return render(request, 'myblog/contact.html')
+
+
+
+
+
+
